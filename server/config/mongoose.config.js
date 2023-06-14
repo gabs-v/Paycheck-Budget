2e27223ea39fb71fcf86be99e9d82dabf92a4701
gabs-v <gabyvazquez8297@gmail.com>
2023-06-13 22:19:47 -0700

    backend create + view all

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -8,4 +8,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/spendingTracker', {
 }) //spendingTracker is the Mongo DB name 
 // The useNewUrlParser and useUnifiedTopology are options we pass to get rid of deprecation messages in our terminal 
     .then(()=>console.log('connected to the spending Tracker whoop whoop!! '))
-    .catch((err) => console.log('oh no! something went wrong connecting to the spending tracker db!!'),err )
+    .catch((err) => console.log('oh no! something went wrong connecting to the spending tracker db!!') )
